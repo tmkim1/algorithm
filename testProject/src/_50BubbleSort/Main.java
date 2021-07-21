42f539988c3208569f6ed5d694203648be343ca0
kimtaemin <xoalstjtm@gmail.com>
2021-07-21 11:02:47 +0900

    버블 정렬 - 큰 값들은 끝에 이미 자리가 고정 되기 때문에 순회할 필요가 없음 해당 부분 수정

--- testProject/src/_50BubbleSort/Main.java
+++ testProject/src/_50BubbleSort/Main.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		int [] numArr = {13,2,15,35,7,5,4,324,1,255};
+		int [] numArr = {13,2,15,35,7,5,4,324,255,1};
 		BubbleSort(numArr);
 		BubbleSort2(numArr);
 	}
@@ -33,11 +33,11 @@ public class Main {
 	public static void BubbleSort2(int[] arr) {
 		int temp = 0;
 		for(int i=0; i<arr.length; i++) {
-			for(int j=1; j<arr.length; j++) {
-				if(arr[j-1] > arr[j]) {
-					temp = arr[j-1];
-					arr[j-1] = arr[j];
-					arr[j] = temp;
+			for(int j=0; j<arr.length-i-1; j++) {
+				if(arr[j] > arr[j+1]) {
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
 			}
 		}
